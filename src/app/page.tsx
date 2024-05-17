5874ae51f6104d6c39ebbab840e447148c85acbb
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2024-05-16 21:34:16 -0300

    refactor: style with tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,28 +14,8 @@ import {
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.container}>
-        <Button variant="destructive">Button</Button>
-      </div>
-      <div className={styles.container}>
-        <AlertDialog>
-          <AlertDialogTrigger>Open</AlertDialogTrigger>
-          <AlertDialogContent>
-            <AlertDialogHeader>
-              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-              <AlertDialogDescription>
-                This action cannot be undone. This will permanently delete your
-                account and remove your data from our servers.
-              </AlertDialogDescription>
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-              <AlertDialogCancel>Cancel</AlertDialogCancel>
-              <AlertDialogAction>Continue</AlertDialogAction>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
-      </div>
-    </main>
+    <div className="bg-blue-500 h-screen w-screen flex items-center justify-center">
+      <div className="bg-white p-16">Centralizada</div>
+    </div>
   )
 }
